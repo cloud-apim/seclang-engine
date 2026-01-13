03f53d9717f2aa38b8369ef52094d569611cec69
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2026-01-13 09:53:32 +0100

    update if found

--- setuptest.sh
+++ setuptest.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-git clone --depth=1 https://github.com/coreruleset/coreruleset.git test-data/coreruleset
+BRANCH="main"
+
+if [ -d "test-data/coreruleset" ]; then
+  cd test-data/coreruleset
+  git checkout $BRANCH
+  git pull origin $BRANCH
+else
+  git clone --depth=1 --branch $BRANCH https://github.com/coreruleset/coreruleset.git test-data/coreruleset
+  git checkout $BRANCH
+fi
